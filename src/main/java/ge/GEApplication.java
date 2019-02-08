8bf24f31a345477222a56b43ad2163c33b20233d
luigimercurio <luigi@luigimercurio.com>
2019-02-08 14:23:25 -0500

    Generalized CSV parser

--- src/main/java/ge/GEApplication.java
+++ src/main/java/ge/GEApplication.java
@@ -1,28 +1,42 @@
 package ge;
 
+import org.merkury.io.IOUtil;
 import org.springframework.boot.ApplicationHome;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class GEApplication extends SpringBootServletInitializer {
-	public static final File HOME;
+	public static final String HOME = new ApplicationHome (GEApplication.class).getDir ().getAbsolutePath ();
 
-	static {
-		HOME = new ApplicationHome (GEApplication.class).getDir ();
-		System.setProperty (GEApplication.class.getSimpleName (), HOME.getAbsolutePath ());
+	protected static Map<String, Object> properties () {
+		String[]            dbInfo;
+		Map<String, Object> properties;
+
+		properties = new HashMap<> ();
+		try {
+			dbInfo = IOUtil.readFromFile (HOME + "/dbinfo").split ("\\|");
+		}
+		catch (Throwable t) {
+			throw new Error (t);
+		}
+		properties.put (GEApplication.class.getSimpleName (), HOME);
+		properties.put ("spring.datasource.url", dbInfo[0]);
+		properties.put ("spring.datasource.username", dbInfo[1]);
+		properties.put ("spring.datasource.password", dbInfo[2]);
+		return properties;
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
-		return application.sources (GEApplication.class);
+		return application.sources (GEApplication.class).properties (properties ());
 	}
 
 	public static void main (String[] args) {
-		SpringApplication.run (GEApplication.class);
+		new SpringApplicationBuilder ().sources (GEApplication.class).properties (properties ()).run ();
 	}
 }
