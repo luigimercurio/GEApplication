b31c73a48c972376b372c22e0ae565d7782e01f0
Luigi Mercurio <luigi@luigimercurio.com>
2019-02-23 10:21:36 -0500

    minor updates to prop2.jst

--- src/main/java/ge/GEApplication.java
+++ src/main/java/ge/GEApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.system.ApplicationHome;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.core.io.ClassPathResource;
 
 import java.util.HashMap;
 import java.util.Map;
